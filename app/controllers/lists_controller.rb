f1cf5ad0e6ac5cae914634210f6a56ee8897bd78
Paul Gamarra <paulgamarra.dev@gmail.com>
2023-11-25 08:25:11 -0300

    before_action :set_list added to lists_controller. Added logic to #show the /list/view bookmarks details only one card

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,12 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
   def index
     @lists = List.all
   end
 
   def show
-    @list = List.find(params[:id])
+
   end
 
   def new
@@ -21,6 +23,10 @@ class ListsController < ApplicationController
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
